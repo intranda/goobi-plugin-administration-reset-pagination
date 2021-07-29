c02ab16ba9d82992285396976b4eb39650e512a0
Oliver Paetzel <oliver.paetzel@intranda.com>
2021-07-29 11:04:53 +0200

    push update to UI

--- plugin/src/main/java/de/intranda/goobi/plugins/ResetPaginationAdministrationPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/ResetPaginationAdministrationPlugin.java
@@ -2,6 +2,8 @@ package de.intranda.goobi.plugins;
 
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.plugin.interfaces.IAdministrationPlugin;
+import org.goobi.production.plugin.interfaces.IPushPlugin;
+import org.omnifaces.cdi.PushContext;
 
 import de.sub.goobi.config.ConfigPlugins;
 import lombok.Getter;
@@ -10,14 +12,14 @@ import net.xeoh.plugins.base.annotations.PluginImplementation;
 
 @PluginImplementation
 @Log4j2
-public class ResetPaginationAdministrationPlugin implements IAdministrationPlugin {
+public class ResetPaginationAdministrationPlugin implements IAdministrationPlugin, IPushPlugin {
 
     @Getter
     private String title = "intranda_administration_reset_pagination";
 
     @Getter
     private String value;
-    
+
     @Getter
     private int counter = 0;
 
@@ -31,6 +33,13 @@ public class ResetPaginationAdministrationPlugin implements IAdministrationPlugi
         return "/uii/plugin_administration_reset_pagination.xhtml";
     }
 
+    private PushContext pusher;
+
+    @Override
+    public void setPushContext(PushContext pusher) {
+        this.pusher = pusher;
+    }
+
     /**
      * Constructor
      */
@@ -38,15 +47,25 @@ public class ResetPaginationAdministrationPlugin implements IAdministrationPlugi
         log.info("Sample admnistration plugin started");
         value = ConfigPlugins.getPluginConfig(title).getString("value", "default value");
     }
-    
+
     public void resetPagination() {
-    	for (int i = 0; i < 100; i++) {
-			counter = i;
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+        Runnable run = () -> {
+            try {
+                for (int i = 0; i < 100; i++) {
+                    counter = i;
+                    Thread.sleep(100);
+                    if (pusher != null && i % 3 == 0) {
+                        pusher.send("update");
+                    }
+                    Thread.sleep(100);
+                    if (pusher != null) {
+                        pusher.send("update");
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        new Thread(run).start();
     }
 }
